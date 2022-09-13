cd8778ffb30c84fa837934b76a5f34717d23da14
Andreas Janz <45030715+janzandr@users.noreply.github.com>
2022-09-13 11:54:21 +0200

    resolved bitbucket-issue-1407

--- enmapbox/coreapps/scatterplotapp/guitest_scatterplotdialog.py
+++ enmapbox/coreapps/scatterplotapp/guitest_scatterplotdialog.py
@@ -1,6 +1,9 @@
+import numpy as np
+
 from qgis.core import QgsRasterLayer
 
 from enmapbox import EnMAPBox, initAll
+from enmapboxprocessing.driver import Driver
 from enmapbox.testing import start_app
 from scatterplotapp.scatterplotdialog import ScatterPlotDialog
 
@@ -14,12 +17,18 @@ layer = QgsRasterLayer(
     'LC08_L2SP_192023_20210724_20210730_02_T1_SR.vrt'
 )
 
+# issue #1407
+filename = r'D:\source\QGISPlugIns\enmap-box\test-outputs\test3.tif'
+Driver(filename).createFromArray(np.array([[[1,2,3,4,5], [11,12,13,14,15], [21,22,23,24,25], [31,32,33,34,35]]]))
+layer = QgsRasterLayer(filename)
+
 mapDock = enmapBox.onDataDropped([layer])
 
 widget = ScatterPlotDialog(enmapBox.ui)
 widget.show()
-widget.mLayer.setLayer(layer)
-widget.mBandX.setBand(4)
-widget.mBandY.setBand(5)
+widget.mLayerX.setLayer(layer)
+widget.mLayerY.setLayer(layer)
+widget.mBandX.setBand(1)
+widget.mBandY.setBand(1)
 
 qgsApp.exec_()
