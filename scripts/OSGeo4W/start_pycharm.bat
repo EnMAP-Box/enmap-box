c08cde0e0d45800df67b98149b52833362c7aa24
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2022-10-08 00:08:30 +0200

    fixed qgis_env.bat call

--- scripts/OSGeo4W/start_pycharm.bat
+++ scripts/OSGeo4W/start_pycharm.bat
@@ -1,4 +1,4 @@
 :: this script stats PyCharm in a requested QGIS environment
 :: change the qgis-env.bat argument to qgis, qgis-dev or qgis-ltr
-call "%~dp0\qgis-env.bat" qgis
+call "%~dp0\qgis_env.bat" qgis
 start "PYCHARM" /B %PYCHARM_EXE%
