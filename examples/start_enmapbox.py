d43aadf3cd68bea568592543fb1d5efee99b3619
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2022-10-17 15:10:45 +0200

    Update start_enmapbox.py

--- examples/start_enmapbox.py
+++ examples/start_enmapbox.py
@@ -5,7 +5,7 @@ from enmapbox.testing import start_app
 qgsApp = start_app()
 
 enmapBox = EnMAPBox()
-#enmapBox.ui.setFixedSize(1920 - 2, 1080 - 32)  # for recording 1080p videos with ScreenToGif
+enmapBox.ui.setFixedSize(1920 - 2, 1080 - 32)  # for recording 1080p videos with ScreenToGif
 
 enmapBox.openExampleData()
 qgsApp.exec_()
