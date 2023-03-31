48ccba462be6d96e08bd0f78664b9eff6f0517e0
Benjamin Jakimow <benjamin.jakimow@geo.hu-berlin.de>
2023-04-01 00:48:00 +0200

    modified runtests.sh

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -3,11 +3,15 @@ QT_QPA_PLATFORM=offscreen
 export QT_QPA_PLATFORM
 CI=True
 export CI
+rm -Rf test-outputs
+rm -Rf test-reports
+export PYTHONPATH="${PYTHONPATH}"\
+":$(pwd)"\
+":/usr/share/qgis/python/plugins"\
+":$(pwd)/enmapbox/apps"\
+":$(pwd)/enmapbox/coreapps"\
+":$(pwd)/enmapbox/eo4qapps"
 
-export PYTHONPATH="${PYTHONPATH}:$(pwd):/usr/share/qgis/python/plugins"
-export PYTHONPATH="${PYTHONPATH}:$(pwd):enmapbox/apps"
-export PYTHONPATH="${PYTHONPATH}:$(pwd):enmapbox/coreapps"
-export PYTHONPATH="${PYTHONPATH}:$(pwd):enmapbox/eo4qapps"
 echo $PYTHONPATH
 pytest
 # python3 scripts/setup_repository.py
