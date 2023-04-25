26bd6cfa64c89a0525f2a21018b23f5f3046eb3a
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2023-04-25 10:10:16 +0200

    update QPS to fix QGIS 3.28 fails edited install.bat
    Signed-off-by: jakimowb <benjamin.jakimow@geo.hu-berlin.de>

--- scripts/OSGeo4W/install.bat
+++ scripts/OSGeo4W/install.bat
@@ -1,6 +1,10 @@
 :: This installs QGIS + required packages using the OSGeo4W installer
 echo "Install QGIS"
+
+:: Packages to install. Available package names see https://download.osgeo.org/osgeo4w/v2/x86_64/setup.ini
 set PACKAGES=qgis setup python3-scipy python3-scikit-learn python3-pip python3-geopandas python3-matplotlib
+
+:: The downloaded OSGeo4W installer exe
 set BIN_INSTALLER=C:\Users\geo_beja\Downloads\osgeo4w-setup.exe
 
 :: available download mirrors? see: https://download.osgeo.org/osgeo4w/ssl/mirrors.lst
@@ -11,6 +15,7 @@ rmdir /S /Q %OSGEO4W_ROOT%
 md %OSGEO4W_ROOT%
 start /B /wait %BIN_INSTALLER% ^
    -kq ^
+   -P %PACKAGES% ^
    --advanced ^
    --arch x86_64 ^
    --site %MIRROR% ^
@@ -19,7 +24,5 @@ start /B /wait %BIN_INSTALLER% ^
    --autoaccept ^
    --upgrade-also ^
    --quiet-mode ^
-   --packages %PACKAGES%
 
-call %OSGEO4W_ROOT%\OSGeo4W.bat
-echo Done
+echo "Installation finished"
