63716b122a9ffe6dd505ef2403c6a2741bd3997a
m.wocher <m.wocher@lmu.de>
2023-05-16 15:05:18 +0200

    Removing local test files from git

--- enmapbox/apps/lmuvegetationapps/Processor/modAL/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import ActiveLearner, Committee, CommitteeRegressor
-
-__all__ = ['ActiveLearner', 'Committee', 'CommitteeRegressor']
--- enmapbox/apps/lmuvegetationapps/Processor/modAL/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .learners import ActiveLearner, BayesianOptimizer, Committee, CommitteeRegressor
-
-__all__ = [
-    'ActiveLearner', 'BayesianOptimizer',
-    'Committee', 'CommitteeRegressor'
-]
