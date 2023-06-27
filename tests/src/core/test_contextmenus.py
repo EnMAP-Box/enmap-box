17671e694b66fd8d9b1f6c959bcbbb37747e1e19
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2023-06-27 15:15:08 +0200

    context menu tests
    Signed-off-by: jakimowb <benjamin.jakimow@geo.hu-berlin.de>

--- tests/src/core/test_contextmenus.py
+++ tests/src/core/test_contextmenus.py
@@ -1,31 +1,36 @@
-from PyQt5.QtWidgets import QMenu
+from qgis.PyQt.QtCore import QEvent, Qt, QPointF
+from qgis.PyQt.QtGui import QMouseEvent
+from qgis.PyQt.QtWidgets import QMenu
 
-from enmapbox.gui.contextmenus import EnMAPBoxContextMenuProvider
-from enmapbox.gui.datasources.datasources import DataSource
-from enmapbox.gui.dataviews.docks import Dock
+from enmapbox.gui.contextmenus import EnMAPBoxAbstractContextMenuProvider, EnMAPBoxContextMenuRegistry
 from enmapbox.gui.enmapboxgui import EnMAPBox
 from enmapbox.gui.mapcanvas import MapCanvas
 from enmapbox.testing import EnMAPBoxTestCase
-from qgis._core import QgsMapLayer, QgsPointXY
+from qgis.core import QgsPointXY
+from qgis.gui import QgsMapMouseEvent
 
 
-class MyProvider(EnMAPBoxContextMenuProvider):
+class MyProvider(EnMAPBoxAbstractContextMenuProvider):
 
-    def __init__(self, callback):
-        self.mCallback = callback
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.mCallbackDataView = None
+        self.mCallbackMapLayer = None
+        self.mCallbackMapCanvas = None
+        self.mCallbackDataSource = None
 
-    def extendDataViewMenu(self, *args, **kwds):
-        self.mCallback(*args, **kwds)
+    def populateDataViewMenu(self, *args, **kwds):
+        self.mCallbackDataView(*args, **kwds)
 
-    def extendMapLayerMenu(self, *args, **kwds):
-        self.mCallback(*args, **kwds)
+    def populateMapLayerMenu(self, *args, **kwds):
+        self.mCallbackMapLayer(*args, **kwds)
         pass
 
-    def extendDataSourceMenu(self, *args, **kwds):
-        self.mCallback(*args, **kwds)
+    def populateDataSourceMenu(self, *args, **kwds):
+        self.mCallbackDataSource(*args, **kwds)
 
-    def extendMapCanvasMenu(self, *args, **kwds):
-        self.mCallback(*args, **kwds)
+    def populateMapCanvasMenu(self, *args, **kwds):
+        self.mCallbackMapCanvas(*args, **kwds)
 
 
 class test_applications(EnMAPBoxTestCase):
@@ -33,18 +38,38 @@ class test_applications(EnMAPBoxTestCase):
     def test_mapcanvas(self):
         eb = EnMAPBox(load_core_apps=False, load_other_apps=False)
 
-        myProvider = MyProvider()
-        eb.registerContextMenuProvider(myProvider)
+        provider = MyProvider()
+        self.assertEqual(provider.enmapBox(), eb)
+        EnMAPBoxContextMenuRegistry.instance().addProvider(provider)
 
-        called = False
-        def test_canvas(menu, canvas, point):
+        s = ""
+        mapDock = eb.createMapDock()
+
+        canvas: MapCanvas = mapDock.mapCanvas()
+        m = QMenu()
+        pt = QPointF(canvas.width() * 0.5, canvas.height() * 0.5)
+        event = QMouseEvent(QEvent.MouseButtonPress, pt, Qt.LeftButton, Qt.LeftButton, Qt.NoModifier)
+        event = QgsMapMouseEvent(canvas, event)
+
+        def callback1(menu, canvas, pos, point):
             self.assertIsInstance(menu, QMenu)
             self.assertIsInstance(canvas, MapCanvas)
             self.assertIsInstance(point, QgsPointXY)
-            called = True
-        myProvider.mCallback = test_canvas
+            self.assertEqual(pos, QPointF)
 
-        mapDock = eb.createMapDock()
+        provider.mCallbackMapCanvas = callback1
+        canvas.mousePressEvent(event)
+        canvas.populateContextMenu(m, event)
 
-        mapDock.mapCanvas()
+        # populate map dock menu
+        m = QMenu()
+
+        def callback2(menu, dataView):
+            self.assertEqual(menu, m)
+            self.assertEqual(dataView, mapDock)
+
+        provider.mCallbackDataView = callback2
+        mapDock.populateContextMenu(m)
+
+        self.showGui(eb.ui)
         eb.close()
