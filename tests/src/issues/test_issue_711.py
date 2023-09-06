5eca7598988416742bd244111e93554333b045ff
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2023-09-06 14:47:42 +0200

    fixed test data paths
    Signed-off-by: Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>

--- tests/src/issues/test_issue_711.py
+++ tests/src/issues/test_issue_711.py
@@ -24,6 +24,7 @@ import enmapbox.exampledata
 import qgis
 from enmapbox.gui.enmapboxgui import EnMAPBox
 from enmapbox.testing import TestObjects, EnMAPBoxTestCase
+from enmapboxtestdata import library_gpkg, enmap_srf_library
 from qgis.core import QgsProject
 
 
@@ -59,8 +60,8 @@ class TestIssue711(EnMAPBoxTestCase):
             sources += [enmapbox.exampledata.enmap,
                         enmapbox.exampledata.landcover_polygon,
                         enmapbox.exampledata.landcover_point,
-                        enmapbox.exampledata.library_gpkg,
-                        enmapbox.exampledata.enmap_srf_library
+                        library_gpkg,
+                        enmap_srf_library
                         ]
 
             sources += [TestObjects.createVectorLayer(),
