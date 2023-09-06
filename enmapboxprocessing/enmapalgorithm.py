b287821d48196d5345f280f24b387a5201f48bdb
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2023-09-06 23:18:22 +0200

    added API switch to remove deprecation warning
    Signed-off-by: jakimowb <benjamin.jakimow@geo.hu-berlin.de>

--- enmapboxprocessing/enmapalgorithm.py
+++ enmapboxprocessing/enmapalgorithm.py
@@ -194,7 +194,10 @@ class EnMAPProcessingAlgorithm(QgsProcessingAlgorithm):
     def parameterAsFields(
             self, parameters: Dict[str, Any], name: str, context: QgsProcessingContext
     ) -> Optional[List[str]]:
-        fields = super().parameterAsFields(parameters, name, context)
+        if Qgis.versionInt() >= 33200:
+            fields = super().parameterAsString(parameters, name, context)
+        else:
+            fields = super().parameterAsFields(parameters, name, context)
         if len(fields) == 0:
             return None
         else:
