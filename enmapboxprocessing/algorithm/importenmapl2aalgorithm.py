02d2c2a969f5802ce6acd5d74063a031d570db97
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2023-09-18 16:33:12 +0200

    resolved #321

--- enmapboxprocessing/algorithm/importenmapl2aalgorithm.py
+++ enmapboxprocessing/algorithm/importenmapl2aalgorithm.py
@@ -42,8 +42,8 @@ class ImportEnmapL2AAlgorithm(EnMAPProcessingAlgorithm):
         return [
             (self._FILE, 'The metadata XML file associated with the product.\n'
                          'Instead of executing this algorithm, '
-                         'you may drag&drop the metadata XML file directly from your system file browser onto '
-                         'the EnMAP-Box map view area.'),
+                         'you may drag&drop the metadata XML file directly from your system file browser '
+                         'a) onto the EnMAP-Box map view area, or b) onto the Sensor Product Import panel.'),
             (self._SET_BAD_BANDS, 'Whether to mark no data bands as bad bands.'),
             (self._EXCLUDE_BAD_BANDS, 'Whether to exclude bands.'),
             (self._DETECTOR_OVERLAP, 'Different options for handling the detector overlap region from 900 to 1000 '
