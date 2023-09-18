9c741c92fe068a8d8a94c40dd9d94e859423f81a
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2023-09-18 11:27:51 +0200

    Update ingest_data.py

--- scripts/EnFireMAP/ingest_data.py
+++ scripts/EnFireMAP/ingest_data.py
@@ -3,13 +3,14 @@ from os import listdir, makedirs, sep
 from os.path import join, isdir, dirname, exists, normpath
 from typing import Optional
 
-from osgeo.gdal import BuildVRT, Warp, WarpOptions, Translate, TranslateOptions
+from osgeo import gdal
 
 from enmapbox.qgispluginsupport.qps.utils import SpatialExtent
 from enmapboxprocessing.algorithm.importenmapl2aalgorithm import ImportEnmapL2AAlgorithm
 from enmapboxprocessing.driver import Driver
 from enmapboxprocessing.rasterreader import RasterReader
-from qgis.core import QgsVectorLayer
+from enmapboxprocessing.rasterwriter import RasterWriter
+from qgis.core import QgsVectorLayer, QgsGeometry
 
 rootData = r'D:\data\EnFireMap\data'
 rootCube = r'D:\data\EnFireMap\cube'
@@ -23,6 +24,8 @@ products = [
     'QL_QUALITY_CLOUDSHADOW.TIF', 'QL_QUALITY_HAZE.TIF', 'QL_QUALITY_SNOW.TIF', 'QL_QUALITY_TESTFLAGS.TIF',
     'QL_SWIR.TIF', 'QL_VNIR.TIF', 'SPECTRAL_IMAGE.vrt'
 ]
+productNoDataValues = defaultdict(lambda: 0)
+productNoDataValues['QL_PIXELMASK.TIF'] = 255
 
 
 def prepareSpectralImages():
@@ -64,12 +67,26 @@ def ingestData():
                 productFilename2 = join(rootTmpWarped, *tmp[-2:])
                 if not exists(dirname(productFilename2)):
                     makedirs(dirname(productFilename2))
-                options = WarpOptions(
-                    format='GTiff', dstSRS=tilingScheme.crs().toWkt(),
-                    creationOptions=Driver.DefaultGTiffCreationOptions
+                # derive correct target grid to avoid later resampling
+                reader = RasterReader(productFilename)
+                extent = SpatialExtent(reader.crs(), reader.extent())
+                extent2 = extent.toCrs(tilingScheme.crs())
+                tilingScheme.selectByRect(extent2)
+                geometries = [feature.geometry() for feature in tilingScheme.selectedFeatures()]
+                bb = QgsGeometry.unaryUnion(geometries).boundingBox()
+                outputBounds = (bb.xMinimum(), bb.yMinimum(), bb.xMaximum(), bb.yMaximum())
+                # warp
+                srcNoData = reader.noDataValue(1)
+                if srcNoData is None:
+                    srcNoData = productNoDataValues[product]
+                options = gdal.WarpOptions(
+                    format='GTiff', dstSRS=tilingScheme.crs().toWkt(), outputBounds=outputBounds,
+                    creationOptions=Driver.DefaultGTiffCreationOptions,
+                    xRes=reader.rasterUnitsPerPixelX(), yRes=reader.rasterUnitsPerPixelY(),
+                    srcNodata=srcNoData
                 )
-                Warp(productFilename2, productFilename, options=options)
-
+                if not exists(productFilename2):
+                    gdal.Warp(productFilename2, productFilename, options=options)
                 filesByDateAndProduct[(datestamp, product)].append(productFilename2)
 
     # - mosaic and cut into tiling scheme
@@ -78,24 +95,34 @@ def ingestData():
         if not exists(join(rootTmpMosaics, datestamp)):
             makedirs(join(rootTmpMosaics, datestamp))
         filename = join(rootTmpMosaics, datestamp, datestamp + '_' + product).replace('.TIF', '.vrt')
-        BuildVRT(filename, filenames)
-
+        if not exists(filename):
+            gdal.BuildVRT(filename, filenames)
         reader = RasterReader(filename)
         extent = SpatialExtent(reader.crs(), reader.extent())
         extent2 = extent.toCrs(tilingScheme.crs())
         tilingScheme.selectByRect(extent2)
         for feature in tilingScheme.selectedFeatures():
             tileName = str(feature.attribute(idField))
-            print('cut', tileName, datestamp + '_' + product, flush=True)
-            filename2 = join(rootCube, tileName, datestamp + '_' + product).replace('.vrt', '.TIF')
+            print('cut', tileName, 'ENMAPL2A_' + datestamp + '_' + product, flush=True)
+            filename2 = join(rootCube, tileName, 'ENMAPL2A_' + datestamp + '_' + product).replace('.vrt', '.TIF')
             if not exists(join(rootCube, tileName)):
                 makedirs(join(rootCube, tileName))
             tileExtent = feature.geometry().boundingBox()
             projWin = tileExtent.xMinimum(), tileExtent.yMaximum(), tileExtent.xMaximum(), tileExtent.yMinimum()
-            options = TranslateOptions(
+            options = gdal.TranslateOptions(
                 format='GTiff', projWin=projWin, creationOptions=Driver.DefaultGTiffCreationOptions
             )
-            Translate(filename2, filename, options=options)
+            if not exists(filename2):
+                ds = gdal.Translate(filename2, filename, options=options)
+
+                if product == 'SPECTRAL_IMAGE.vrt':
+                    # copy metadata
+                    reader = RasterReader(filesByDateAndProduct[(datestamp, 'SPECTRAL_IMAGE.vrt')][0])
+                    writer = RasterWriter(ds)
+                    writer.setMetadata(reader.metadata())
+                    for bandNo in reader.bandNumbers():
+                        writer.setMetadata(reader.metadata(bandNo), bandNo)
+                        writer.setBandName(reader.bandName(bandNo), bandNo)
 
 
 def auxFindMetadataXml(folder: str) -> Optional[str]:
