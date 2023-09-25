99abbbc3916e63a78202a30d600b2416c11b0a57
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2023-09-25 10:05:17 +0200

    resolved #580

---
+++ tests/enmap-box/enmapboxprocessing/algorithm/test_SpectralResamplingByWavelengthAlgorithm.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+from enmapboxprocessing.algorithm.spectralresamplingbywavelengthalgorithm import SpectralResamplingByWavelengthAlgorithm
+from enmapboxprocessing.algorithm.testcase import TestCase
+from enmapboxprocessing.rasterreader import RasterReader
+from enmapboxtestdata import enmap, envi_library_berlin_sli, enmap_berlin_srf_csv, classificationDatasetAsPklFile, \
+    enmap_potsdam
+
+
+class TestSpectralResamplingByWavelengthAlgorithm(TestCase):
+
+    def test_fromSameRaster(self):  # this is an edge case
+        alg = SpectralResamplingByWavelengthAlgorithm()
+        parameters = {
+            alg.P_RASTER: enmap,
+            alg.P_WAVELENGTH_FILE: enmap,
+            alg.P_OUTPUT_RASTER: self.filename('resampled.tif')
+        }
+        result = self.runalg(alg, parameters)
+        self.assertEqual(29424494, np.round(np.sum(RasterReader(result[alg.P_OUTPUT_RASTER]).array()[0])))
+
+    def test_fromDifferentRaster(self):
+        alg = SpectralResamplingByWavelengthAlgorithm()
+        parameters = {
+            alg.P_RASTER: enmap,
+            alg.P_WAVELENGTH_FILE: enmap_potsdam,
+            alg.P_OUTPUT_RASTER: self.filename('resampled2.tif')
+        }
+        result = self.runalg(alg, parameters)
+        self.assertEqual(29424494, np.round(np.sum(RasterReader(result[alg.P_OUTPUT_RASTER]).array()[0])))
+
+    def test_fromEnviSpeclib(self):
+        alg = SpectralResamplingByWavelengthAlgorithm()
+        parameters = {
+            alg.P_RASTER: enmap,
+            alg.P_WAVELENGTH_FILE: envi_library_berlin_sli,
+            alg.P_OUTPUT_RASTER: self.filename('resampled.tif')
+        }
+        result = self.runalg(alg, parameters)
+        self.assertEqual(29424494, np.round(np.sum(RasterReader(result[alg.P_OUTPUT_RASTER]).array()[0])))
+
+    def test_fromEnviSpeclibHeader(self):
+        alg = SpectralResamplingByWavelengthAlgorithm()
+        parameters = {
+            alg.P_RASTER: enmap,
+            alg.P_WAVELENGTH_FILE: envi_library_berlin_sli.replace('.sli', '.hdr'),
+            alg.P_OUTPUT_RASTER: self.filename('resampled.tif')
+        }
+        result = self.runalg(alg, parameters)
+        self.assertEqual(29424494, np.round(np.sum(RasterReader(result[alg.P_OUTPUT_RASTER]).array()[0])))
+
+    def test_fromEnviRasterHeader(self):
+        alg = SpectralResamplingByWavelengthAlgorithm()
+        parameters = {
+            alg.P_RASTER: enmap,
+            alg.P_WAVELENGTH_FILE: enmap.replace('.bsq', '.hdr'),
+            alg.P_OUTPUT_RASTER: self.filename('resampled.tif')
+        }
+        result = self.runalg(alg, parameters)
+        self.assertEqual(29424494, np.round(np.sum(RasterReader(result[alg.P_OUTPUT_RASTER]).array()[0])))
+
+    def test_fromCsvTable(self):
+        alg = SpectralResamplingByWavelengthAlgorithm()
+        parameters = {
+            alg.P_RASTER: enmap,
+            alg.P_WAVELENGTH_FILE: enmap_berlin_srf_csv,
+            alg.P_OUTPUT_RASTER: self.filename('resampled.tif')
+        }
+        result = self.runalg(alg, parameters)
+        self.assertEqual(29424494, np.round(np.sum(RasterReader(result[alg.P_OUTPUT_RASTER]).array()[0])))
+
+    def test_wrongResponseFile(self):
+        alg = SpectralResamplingByWavelengthAlgorithm()
+        parameters = {
+            alg.P_RASTER: enmap,
+            alg.P_WAVELENGTH_FILE: classificationDatasetAsPklFile,  # PKL files aren't valid response files
+            alg.P_OUTPUT_RASTER: self.filename('resampled.tif')
+        }
+        try:
+            self.runalg(alg, parameters)
+            assert 0, 'error not raised, something went wrong'
+        except Exception as error:
+            pass
