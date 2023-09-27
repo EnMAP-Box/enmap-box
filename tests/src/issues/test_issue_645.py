37e22563fbd861db4af667c8dbcd708ca2f6cb38
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2023-09-27 08:06:49 +0200

    added tests for #645
    Signed-off-by: jakimowb <benjamin.jakimow@geo.hu-berlin.de>

--- tests/src/issues/test_issue_645.py
+++ tests/src/issues/test_issue_645.py
@@ -1,10 +1,17 @@
+from PyQt5.QtWidgets import QWidget
+
 from enmapbox import initAll
 from enmapbox.gui.dataviews.docks import SpectralLibraryDock
 from enmapbox.gui.enmapboxgui import EnMAPBox
 from enmapbox.qgispluginsupport.qps.speclib.gui.spectralprocessingdialog import SpectralProcessingDialog
 from enmapboxprocessing.testcase import TestCase
+from processing.gui.wrappers import WidgetWrapperFactory
+from qgis._core import QgsProcessingContext, QgsProject, QgsProcessingRegistry, QgsApplication, QgsProcessingAlgorithm
+from qgis._gui import QgsProcessingAlgorithmDialogBase
 from qgis.core import QgsVectorLayer
 from enmapboxtestdata import library_berlin
+from qps.speclib.gui.spectralprocessingdialog import SpectralProcessingModelCreatorAlgorithmWrapper
+
 
 class Issue645Tests(TestCase):
 
@@ -12,7 +19,6 @@ class Issue645Tests(TestCase):
 
         initAll()
         box = EnMAPBox()
-
 
         sl = QgsVectorLayer(library_berlin)
         sl.startEditing()
@@ -23,9 +29,41 @@ class Issue645Tests(TestCase):
     def test_issue_dialog(self):
 
         initAll()
-
         sl = QgsVectorLayer(library_berlin)
         sl.startEditing()
         d = SpectralProcessingDialog(speclib=sl)
         d.setAlgorithm('enmapbox:TranslateRasterLayer')
         self.showGui(d)
+
+    def test_wrapper(self):
+
+        initAll()
+        base = TestBase()
+        a = 'enmapbox:TranslateRasterLayer'
+        # a = 'gdal:translate'
+        reg: QgsProcessingRegistry = QgsApplication.instance().processingRegistry()
+        alg: QgsProcessingAlgorithm = reg.algorithmById(a)
+        self.assertIsInstance(alg, QgsProcessingAlgorithm)
+        sl = QgsVectorLayer(library_berlin)
+        sl.startEditing()
+        context = QgsProcessingContext()
+        project = QgsProject()
+        context.setProject(project)
+        p = QWidget()
+        w = SpectralProcessingModelCreatorAlgorithmWrapper(alg, sl, context, parent=p)
+        w.initWidgets()
+        self.showGui(w)
+
+        p2 = QWidget()
+        # 'createProfile'
+        for param in alg.parameterDefinitions():
+            if param.name() == 'createProfile':
+                wrapper = WidgetWrapperFactory.create_wrapper_from_metadata(param, p2, row=0, col=0)
+                s = ""
+        self.showGui(p)
+
+
+class TestBase(QgsProcessingAlgorithmDialogBase):
+
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
