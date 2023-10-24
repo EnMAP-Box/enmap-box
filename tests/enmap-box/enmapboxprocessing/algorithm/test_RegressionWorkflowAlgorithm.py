762b2713516e81d7a22b6178eba4e93ec9af0fe8
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2023-10-24 11:13:41 +0200

    fixed test

--- tests/enmap-box/enmapboxprocessing/algorithm/test_RegressionWorkflowAlgorithm.py
+++ tests/enmap-box/enmapboxprocessing/algorithm/test_RegressionWorkflowAlgorithm.py
@@ -5,7 +5,7 @@ from enmapboxprocessing.algorithm.prepareregressiondatasetfromcontinuousvectoral
     PrepareRegressionDatasetFromContinuousVectorAlgorithm
 from enmapboxprocessing.algorithm.regressionworkflowalgorithm import RegressionWorkflowAlgorithm
 from enmapboxprocessing.algorithm.testcase import TestCase
-from enmapboxtestdata import enmap, enmap_potsdam
+from enmapboxtestdata import enmap, enmap_potsdam, veg_cover_fraction_potsdam_point
 from enmapboxtestdata import regressorDumpMultiTargetPkl
 
 
