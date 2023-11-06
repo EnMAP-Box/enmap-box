6c21039499f750acdf41476ad83f8aed400fddcc
Stefan Ernst <15325433+ernstste@users.noreply.github.com>
2023-11-06 11:57:49 +0100

    linting for urlcheck

--- enmapbox/coreapps/_classic/enmapboxgeoalgorithms/estimators/regressors/LinearRegressionHelp.py
+++ enmapbox/coreapps/_classic/enmapboxgeoalgorithms/estimators/regressors/LinearRegressionHelp.py
@@ -4,5 +4,5 @@ helpAlg = Help(text='Fits a Linear Regression.')
 
 helpCode = Help(text='Scikit-learn python code. See {} for information on different parameters. See {} for information on scaling.',
                 links=[Link('http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html', 'LinearRegression'),
-                       Link('http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html','StandardScaler')
+                       Link('http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html', 'StandardScaler')
                        ])
