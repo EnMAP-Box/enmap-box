4f8fa95c5cc297a2c1991a96e49777e2ff92f756
Benjamin Jakimow <benjamin.jakimow@geo.hu-berlin.de>
2023-11-08 18:35:36 +0100

    refactored DASF tests

--- enmapbox/apps/DASFEnMAPbox/test_app.py
+++ enmapbox/apps/DASFEnMAPbox/test_app.py
@@ -1,13 +1,24 @@
-from enmapbox.gui.enmapboxgui import EnMAPBox
-from enmapbox.testing import start_app
-from DASFEnMAPbox import DASFretrievalApp
+import unittest
+from enmapbox.testing import start_app, TestCase
+from enmapbox import registerEnMAPBoxProcessingProvider
+
+start_app()
+registerEnMAPBoxProcessingProvider()
+
+
+class DASFTests(TestCase):
+
+    def test_dasf(self):
+        from enmapbox.gui.enmapboxgui import EnMAPBox
+        enmapBox = EnMAPBox()
+        enmapBox.run()
+        enmapBox.openExampleData(mapWindows=1)
+        from DASFEnMAPbox import DASFretrievalApp
+        enmapBox.addApplication(DASFretrievalApp(enmapBox=enmapBox))
+
+        self.showGui(enmapBox.ui)
+        enmapBox.close()
+
 
 if __name__ == '__main__':
-
-    qgsApp = start_app()
-    enmapBox = EnMAPBox(None)
-    enmapBox.run()
-    enmapBox.openExampleData(mapWindows=1)
-    enmapBox.addApplication(DASFretrievalApp(enmapBox=enmapBox))
-    qgsApp.exec_()
-    qgsApp.exitQgis()
+    unittest.main(buffer=False)
