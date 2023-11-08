e7b25f12f332f3d36c8480eae52f188e234d0443
Benjamin Jakimow <benjamin.jakimow@geo.hu-berlin.de>
2023-11-08 18:53:57 +0100

    improved testing routines (call start_app in beginning)

--- enmapbox/apps/DASFEnMAPbox/test_processingalgorithm.py
+++ enmapbox/apps/DASFEnMAPbox/test_processingalgorithm.py
@@ -1,7 +1,10 @@
+import os
+import warnings
 from os.path import join, dirname
 from qgis.core import QgsProcessingFeedback, QgsApplication
 from processing.core.Processing import Processing
 from DASFEnMAPbox.processingalgorithm import DASFretrievalAlgorithm
+
 import enmapboxtestdata
 
 # init QGIS
@@ -12,17 +15,21 @@ qgsApp.messageLog().messageReceived.connect(lambda *args: print(args[0]))
 # init processing framework
 Processing.initialize()
 
+path = r"C:\Users\Marion\Desktop\EnMAP_box\MyTool\KROOF03_Viertel_RFC_geo_1m_subset710_790.bsq"
 # run algorithm
-alg = DASFretrievalAlgorithm()
-io = {alg.P_INPUT: "C:\\Users\\Marion\\Desktop\\EnMAP_box\\MyTool\\KROOF03_Viertel_RFC_geo_1m_subset710_790.bsq",
-      alg.P_OUTPUT: join(dirname(__file__), 'DASF.bsq'),
-      alg.P_Retrieval_Quality: join(dirname(__file__), 'DASF_retrievalQuality.bsq'),
-      alg.P_CSC: join(dirname(__file__), 'CSC.bsq')}
-result = Processing.runAlgorithm(alg, parameters=io)
+if not os.path.isfile(path):
+    warnings.warn(f'File does not exist: {path}\n skip DASF tests')
+else:
+    alg = DASFretrievalAlgorithm()
+    io = {alg.P_INPUT: path,
+          alg.P_OUTPUT: join(dirname(__file__), 'DASF.bsq'),
+          alg.P_Retrieval_Quality: join(dirname(__file__), 'DASF_retrievalQuality.bsq'),
+          alg.P_CSC: join(dirname(__file__), 'CSC.bsq')}
+    result = Processing.runAlgorithm(alg, parameters=io)
 
-print(result)
+    print(result)
 
-if True: # show the result in a viewer
-    from _classic.hubdc.core import MapViewer, openRasterDataset
-    fraction = openRasterDataset(result[alg.P_OUTPUT])
-    MapViewer().addLayer(fraction.mapLayer().initMultiBandColorRenderer(0, 2, 4, percent=0)).show()
+    if True: # show the result in a viewer
+        from _classic.hubdc.core import MapViewer, openRasterDataset
+        fraction = openRasterDataset(result[alg.P_OUTPUT])
+        MapViewer().addLayer(fraction.mapLayer().initMultiBandColorRenderer(0, 2, 4, percent=0)).show()
