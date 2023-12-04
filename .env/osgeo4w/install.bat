8fd1675767378709a3c5e246636208369c09dfbe
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2023-12-04 14:19:00 +0100

    OSGeo4W requirements: moved OpenGL to install.bat

--- .env/osgeo4w/install.bat
+++ .env/osgeo4w/install.bat
@@ -2,11 +2,11 @@
 echo "Install QGIS"
 
 :: Packages to install. Available package names see https://download.osgeo.org/osgeo4w/v2/x86_64/setup.ini
-set PACKAGES=qgis setup python3-scipy python3-scikit-learn python3-pip python3-geopandas python3-matplotlib
+set PACKAGES=qgis setup python3-scipy python3-scikit-learn python3-pip python3-geopandas python3-matplotlib python3-pyopengl
 
 :: The downloaded OSGeo4W installer exe
 set BIN_INSTALLER=C:\Users\geo_beja\Downloads\osgeo4w-setup.exe
-
+set BIN_INSTALLER=setup.bat
 :: available download mirrors? see: https://download.osgeo.org/osgeo4w/ssl/mirrors.lst
 set MIRROR=https://download.osgeo.org/osgeo4w/v2
 
@@ -24,5 +24,7 @@ start /B /wait %BIN_INSTALLER% ^
    --autoaccept ^
    --upgrade-also ^
    --quiet-mode ^
+
+start /B /wait %BIN_INSTALLER% -kq -P %PACKAGES% --advanced --arch x86_64 --site %MIRROR% --only-site --autoaccept --upgrade-also --quiet-mode
 
 echo "Installation finished"
