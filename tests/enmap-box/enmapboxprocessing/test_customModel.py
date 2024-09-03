816389190862785131f5f5ee54ee6e993c2a643c
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2024-09-03 16:17:14 +0200

    Update test_customModel.py

--- tests/enmap-box/enmapboxprocessing/test_customModel.py
+++ tests/enmap-box/enmapboxprocessing/test_customModel.py
@@ -4,13 +4,16 @@ from shutil import copyfile
 from enmapbox import initAll
 from enmapbox.testing import start_app
 from enmapboxprocessing.algorithm.testcase import TestCase
-from enmapboxtestdata import custom_model
+from enmapboxtestdata import custom_model, SensorProducts, sensorProductsRoot
 from qgis.core import QgsApplication
 
 
 class TestCustomModel(TestCase):
 
     def test(self):
+        if sensorProductsRoot() is None or self.skipProductImport:
+            return
+
         profileFolder = join(QgsApplication.qgisSettingsDirPath(), 'processing', 'models')
         copyfile(custom_model, join(profileFolder, basename(custom_model)))
 
@@ -19,7 +22,7 @@ class TestCustomModel(TestCase):
 
         alg = 'model:importEnmapL2'
         parameters = {
-            'enmap_l2_xml_file': r'D:\data\sensors\enmap\ENMAP01-____L2A-DT0000004135_20221005T023547Z_010_V010106_20221014T102749Z\ENMAP01-____L2A-DT0000004135_20221005T023547Z_010_V010106_20221014T102749Z-METADATA.XML',
+            'enmap_l2_xml_file': SensorProducts.Enmap.L2A_MetadataXml,
             'enmap_l2_raster': self.filename('enmalL2.vrt')
         }
         self.runalg(alg, parameters)
