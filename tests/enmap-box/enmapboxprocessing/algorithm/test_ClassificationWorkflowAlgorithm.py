4b32f489a7c34a62040d30cf6bb33506f7c2e189
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2024-12-18 14:28:12 +0100

    Update test_ClassificationWorkflowAlgorithm.py

--- tests/enmap-box/enmapboxprocessing/algorithm/test_ClassificationWorkflowAlgorithm.py
+++ tests/enmap-box/enmapboxprocessing/algorithm/test_ClassificationWorkflowAlgorithm.py
@@ -1,3 +1,5 @@
+import unittest
+
 from sklearn.base import ClassifierMixin
 
 from enmapbox import initAll
@@ -29,7 +31,7 @@ class FitTestClassifierAlgorithm(FitClassifierAlgorithmBase):
         classifier = RandomForestClassifier(n_estimators=10, oob_score=True, random_state=42)
         return classifier
 
-
+@unittest.skipIf(gdal.VersionInfo().startswith('310'), 'Rasterize decimal error')
 class TestClassificationWorkflowAlgorithm(TestCase):
 
     def test(self):
