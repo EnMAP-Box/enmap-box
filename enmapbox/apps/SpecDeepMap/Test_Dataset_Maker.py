ff52a5b27dc17ecb21664669a271d7f8baf98379
thomaslfEO <93778930+thomaslf-eo@users.noreply.github.com>
2025-02-21 17:05:20 +0100

    Test Dataset Maker

---
+++ enmapbox/apps/SpecDeepMap/Test_Dataset_Maker.py
@@ -0,0 +1,75 @@
+from os.path import join, dirname
+from qgis.core import QgsProcessingFeedback, QgsApplication
+from processing.core.Processing import Processing
+import pandas as pd
+
+from enmapbox.apps.SpecDeepMap.processingalgorithmDS_SUM12 import DatasetSplitter_SUM
+
+from enmapbox import exampledata
+
+import glob
+from enmapboxprocessing.testcase import TestCase
+
+class Test_Dataset_Maker(TestCase):
+
+    def test_init(self):
+
+      # init QGIS
+      qgsApp = QgsApplication([], True)
+      qgsApp.initQgis()
+      qgsApp.messageLog().messageReceived.connect(lambda *args: print(args[0]))
+
+      # init processing framework
+      Processing.initialize()
+
+      # run algorithm
+      alg = DatasetSplitter_SUM()
+
+      # Get the script's directory (makes paths relative)
+      BASE_DIR = dirname(__file__)
+
+      folder_path = join(BASE_DIR, "test_run/")
+
+
+      io = {alg.Train_Val_folder: folder_path,
+            alg.N_train:80,
+            alg.N_test: 10,
+            alg.N_val: 10,
+            alg.Seed: 42,
+            alg.scaler: 10000,
+            alg.normalize: True,
+            alg.N_permute: 100,
+            alg.Output_path: folder_path}
+
+      result = Processing.runAlgorithm(alg, parameters=io)
+
+      print(result)
+
+       # Test all Csv files created
+      csv_files = glob.glob(f"{folder_path}/*.csv")
+      num_csv_files = len(csv_files)# List all .tif files
+      assert num_csv_files == 5, f"Error: Expected 18 .tif files, but found {num_csv_files}"
+
+      # Test split correct Train, Test, Val
+
+      csv_files = {
+          "train_files.csv": 14,
+          "test_files.csv": 2,
+          "validation_files.csv": 2
+      }
+
+      # Loop through each CSV file and check the row count
+      for csv_file, expected_count in csv_files.items():
+          path_csv_file = join(folder_path, csv_file)
+          df = pd.read_csv(path_csv_file)
+
+          row_count = df['image'].count()
+          assert row_count == expected_count, f"Error: Expected {expected_count} .tif files in {csv_file}, but found {row_count}"
+
+      # add test summary csv
+
+
+
+
+
+      #add test normalization
