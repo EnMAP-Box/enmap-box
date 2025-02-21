31362e12355cc1e934b1c1ecb74c063fedcee99e
thomaslfEO <93778930+thomaslf-eo@users.noreply.github.com>
2025-02-21 16:38:24 +0100

    Test Rastersplitter

---
+++ enmapbox/apps/SpecDeepMap/Test_raster_splitter.py
@@ -0,0 +1,64 @@
+from os.path import join, dirname
+from qgis.core import QgsProcessingFeedback, QgsApplication
+from processing.core.Processing import Processing
+
+
+from enmapbox.apps.SpecDeepMap.processingalgorithmRSRASTER_PERCENT7 import RasterSplitterRP
+
+from enmapbox import exampledata
+
+import glob
+
+from enmapboxprocessing.testcase import TestCase
+
+
+class TestRasterReader(TestCase):
+    def test_init(self):
+
+      # init QGIS
+      qgsApp = QgsApplication([], True)
+      qgsApp.initQgis()
+      qgsApp.messageLog().messageReceived.connect(lambda *args: print(args[0]))
+
+      # init processing framework
+      Processing.initialize()
+
+      # run algorithm
+      alg = RasterSplitterRP()
+
+      INPUT_I = 'INPUT_I'
+      INPUT_L = 'INPUT_L'
+      P_tile_x = 'tileSize_x'
+      P_tile_y = 'tileSize_y'
+      P_step_x = 'stepSize_x'
+      P_step_y = 'stepSize_y'
+      P_OUTPUT_F: str = 'OutputFolder'
+      # P_removenull = 'P_removenull'
+      Percent_null = 'Percent_null'
+
+      # Get the script's directory (makes paths relative)
+      BASE_DIR = dirname(__file__)
+
+      # Define paths using relative references
+      input_l_path = join(BASE_DIR, "test_run/enmap_landcover_unstyled.tif")
+      folder_path = join(BASE_DIR, "test_run/")
+      folder_path_images = join(BASE_DIR, "test_run/images/")
+
+
+      io = {alg.INPUT_I: exampledata.enmap,
+            alg.INPUT_L: input_l_path,
+            alg.P_tile_x: 32,
+            alg.P_tile_y: 32,
+            alg.P_step_x: 32,
+            alg.P_step_y: 32,
+            alg.Percent_null: 10,
+            alg.P_OUTPUT_F: folder_path}
+
+      result = Processing.runAlgorithm(alg, parameters=io)
+
+      print(result)
+
+       # Change to your folder path
+      tif_files = glob.glob(f"{folder_path_images}/*.tif")
+      num_tif_files = len(tif_files)# List all .tif files
+      assert num_tif_files == 18, f"Error: Expected 18 .tif files, but found {num_tif_files}"
