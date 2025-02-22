d31de8c1dbc186e3727b46b418b1595579fd0a27
thomaslfEO <93778930+thomaslf-eo@users.noreply.github.com>
2025-02-22 16:30:37 +0100

    Test Deep Learning trainer 2

--- enmapbox/apps/SpecDeepMap/Test_Deep_Learning_Trainer.py
+++ enmapbox/apps/SpecDeepMap/Test_Deep_Learning_Trainer.py
@@ -45,11 +45,16 @@ class Test_Deep_Learning_Trainer(TestCase):
 
         folder_path = join(BASE_DIR, "test_run/")
 
+        # delete checkpoint files from previous test
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".ckpt"):  # Assuming checkpoint files have a .ckpt extension
+                file_path = os.path.join(folder_path, filename)
+                os.remove(file_path)
 
         io = {alg.train_val_input_folder: folder_path,
                 alg.arch:3,
                 alg.backbone: 'resnet18',
-                alg.pretrained_weights: None,
+                alg.pretrained_weights: 1,
                 alg.freeze_encoder: False,
                 alg.data_aug: True,
                 alg.batch_size: 2,
@@ -98,5 +103,64 @@ class Test_Deep_Learning_Trainer(TestCase):
         assert isinstance(model_loaded.hparams.transform,v2.Compose), f'Data augmentation should be torchvision Compose, but is , it is {type(model_loaded.hparams.transform)}'
         assert isinstance(model_loaded.hparams.preprocess,transforms.Compose), f'Preprocess should be torchvision Compose, but is , it is {type(model_loaded.hparams.preprocess)}'
 
-        # 4. Test check if pretrained model runs
+        # delete checkpoint files from previous test
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".ckpt"):  # Assuming checkpoint files have a .ckpt extension
+                file_path = os.path.join(folder_path, filename)
+                os.remove(file_path)
 
+
+        # 4. Test check if pretrained model runs needs 13 channel adjust indexing before splitting raster
+        io = {alg.train_val_input_folder: folder_path,
+              alg.arch: 0,
+              alg.backbone: 'resnet18',
+              alg.pretrained_weights: 0,
+              alg.freeze_encoder: True,
+              alg.data_aug: True,
+              alg.batch_size: 2,
+              alg.n_epochs: 2,
+              alg.lr: 0.001,
+              alg.lr_finder: False,
+              alg.pat: True,
+              alg.class_weights_balanced: True,
+              alg.normalization_flag: True,
+              alg.device: 0,
+              alg.num_workers: 0,
+              alg.device_numbers: 1,
+              alg.num_models: -1,
+              alg.tensorboard: False,
+              alg.logdirpath: folder_path,
+              alg.logdirpath_model: folder_path,
+              }
+
+        result = Processing.runAlgorithm(alg, parameters=io)
+
+        best = best_ckpt_path(folder_path)
+
+        model_loaded = MyModel.load_from_checkpoint(best)
+
+        # 2. Test check if model has subclass, pytorch lightning Module
+        is_inherited = isinstance(model_loaded, L.LightningModule)
+        assert is_inherited == True, "Model did not inherite Lightning Module"
+
+        # 3. Test: check if hparams used in training have been passed correctly to class and checkpoint saver
+
+        print(model_loaded.hparams)
+
+        assert 'Unet' == model_loaded.hparams.architecture, 'Model architecture not passed correctly'
+        assert 'resnet18' == model_loaded.hparams.backbone, 'Model backbone not passed correctly'
+        assert 'imagenet' == model_loaded.hparams.weights, 'Model backbone not passed correctly'
+        #assert all(not param.requires_grad for param in model_loaded.backbone.parameters()), "Error: The model's backbone is not frozen."
+
+        #actual_backbone = getattr(model_loaded, 'backbone', None)  # Replace 'backbone_model' if needed
+        #assert actual_backbone is not None, "Backbone model not found"
+
+        # Check if backbone is frozen
+        #frozen = all(not param.requires_grad for param in actual_backbone.parameters())
+        #assert frozen, "Error: The model's backbone is not frozen."
+
+        # delete checkpoint files from previous test
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".ckpt"):  # Assuming checkpoint files have a .ckpt extension
+                file_path = os.path.join(folder_path, filename)
+                os.remove(file_path)
