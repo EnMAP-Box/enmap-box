fa4185535c0247a23fd56a7aa5a14268c056dd05
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2025-03-30 18:08:45 +0200

    added LMU test, set multiprocessing to threads
    Signed-off-by: jakimowb <benjamin.jakimow@geo.hu-berlin.de>

---
+++ tests/enmap-box/enmapbox/apps/lmuvegetationapps/test_processor.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+
+from qgis.core import QgsApplication
+from enmapboxprocessing.testcase import TestCase
+from lmuvegetationapps.Processor.Processor_Training_GUI import MainUiFunc, ML_Training, MLTrainingGUI
+
+DIR_TESTDATA = Path(__file__).parent / 'data'
+
+
+class LMUTests_Processor(TestCase):
+
+    def test_processor_training(self):
+        path_lut = DIR_TESTDATA / 'TestLUT_2000_CpCBCcheck_00meta.lut'
+
+        dir_outputs = self.createTestOutputDirectory()
+        os.makedirs(dir_outputs, exist_ok=True)
+
+        main = MainUiFunc()
+        training: ML_Training = main.mlra_training
+        gui: MLTrainingGUI = training.gui
+
+        # set parameters in GUI
+        training.open_lut(path=str(path_lut))
+        training.gui.radNoiseOff.click()
+        training.gui.chkAGBdry.setChecked(True)
+        training.gui.rbGPR.click()
+        training.gui.radAL.click()
+        training.gui.txtTrainSize.setText('50')
+
+        training.gui.txtModelDir.setText(dir_outputs.as_posix())
+        training.out_dir = training.gui.txtModelDir.text()
+        training.gui.txtModelName.setText("test")
+        training.model_name = training.gui.txtModelName.text()
+
+        training.gui.show()
+        if False:
+            training.run_training()
+        self.showGui(gui)
+        QgsApplication.exec_()
