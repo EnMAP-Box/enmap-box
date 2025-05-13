e41aaa42695b0532c8c9b62390d7bdfe9de5e239
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2025-05-13 14:54:00 +0200

    Update test_PrepareRegressionDatasetFromContinuousVectorAlgorithm.py

--- tests/enmap-box/enmapboxprocessing/algorithm/test_PrepareRegressionDatasetFromContinuousVectorAlgorithm.py
+++ tests/enmap-box/enmapboxprocessing/algorithm/test_PrepareRegressionDatasetFromContinuousVectorAlgorithm.py
@@ -1,6 +1,4 @@
-import unittest
-
-from osgeo import gdal
+from qgis.core import QgsVectorLayer, QgsProcessingException
 
 from enmapbox import initAll
 from enmapbox.qgispluginsupport.qps.qgsrasterlayerproperties import QgsRasterLayerSpectralProperties
@@ -12,7 +10,6 @@ from enmapboxprocessing.typing import RegressorDump
 from enmapboxprocessing.utils import Utils
 from enmapboxtestdata import enmap_potsdam, landcover_potsdam_point, veg_cover_fraction_potsdam_point
 from enmapboxtestdata import fraction_point_multitarget, fraction_point_singletarget, enmap, landcover_polygon
-from qgis.core import QgsVectorLayer, QgsProcessingException
 
 initAll()
 
@@ -22,57 +19,41 @@ class TestPrepareRegressionDatasetFromContinuousVectorAlgorithm_2(TestCase):
     def test_styled_multitarget(self):
 
         alg = PrepareRegressionDatasetFromContinuousVectorAlgorithm()
-        parameters = {
-            alg.P_FEATURE_RASTER: enmap,
-            alg.P_CONTINUOUS_VECTOR: fraction_point_multitarget,
-            alg.P_OUTPUT_DATASET: self.filename('sample.pkl')
-        }
+        parameters = {alg.P_FEATURE_RASTER: enmap, alg.P_CONTINUOUS_VECTOR: fraction_point_multitarget,
+                      alg.P_OUTPUT_DATASET: self.filename('sample.pkl')}
         self.runalg(alg, parameters)
         dump = RegressorDump.fromDict(Utils.pickleLoad(parameters[alg.P_OUTPUT_DATASET]))
         self.assertEqual((51, 177), dump.X.shape)
         self.assertEqual((51, 6), dump.y.shape)
         self.assertEqual(177, len(dump.features))
         self.assertEqual(['band 8 (0.460000 Micrometers)', 'band 9 (0.465000 Micrometers)'], dump.features[:2])
-        self.assertListEqual(
-            ['roof', 'pavement', 'low vegetation', 'tree', 'soil', 'water'], [t.name for t in dump.targets]
-        )
+        self.assertListEqual(['roof', 'pavement', 'low vegetation', 'tree', 'soil', 'water'],
+                             [t.name for t in dump.targets])
 
         # check locations
         alg = LibraryFromRegressionDatasetAlgorithm()
-        parameters = {
-            alg.P_DATASET: self.filename('sample.pkl'),
-            alg.P_OUTPUT_LIBRARY: self.filename('library.gpkg')
-        }
+        parameters = {alg.P_DATASET: self.filename('sample.pkl'), alg.P_OUTPUT_LIBRARY: self.filename('library.gpkg')}
         self.runalg(alg, parameters)
-        self.assertEqual(
-            384027,
-            round(QgsVectorLayer(self.filename('library.gpkg')).getFeatures().__next__().geometry().asPoint().x())
-        )
+        self.assertEqual(384027,
+                         round(QgsVectorLayer(
+                             self.filename('library.gpkg')).getFeatures().__next__().geometry().asPoint().x()))
 
     def test_styled_singletarget(self):
         alg = PrepareRegressionDatasetFromContinuousVectorAlgorithm()
-        parameters = {
-            alg.P_FEATURE_RASTER: enmap,
-            alg.P_CONTINUOUS_VECTOR: fraction_point_singletarget,
-            alg.P_OUTPUT_DATASET: self.filename('sample.pkl')
-        }
+        parameters = {alg.P_FEATURE_RASTER: enmap, alg.P_CONTINUOUS_VECTOR: fraction_point_singletarget,
+                      alg.P_OUTPUT_DATASET: self.filename('sample.pkl')}
         self.runalg(alg, parameters)
         dump = RegressorDump.fromDict(Utils.pickleLoad(parameters[alg.P_OUTPUT_DATASET]))
         self.assertEqual((51, 177), dump.X.shape)
         self.assertEqual((51, 1), dump.y.shape)
         self.assertEqual(177, len(dump.features))
         self.assertEqual(['band 8 (0.460000 Micrometers)', 'band 9 (0.465000 Micrometers)'], dump.features[:2])
-        self.assertListEqual(
-            ['vegetation'], [t.name for t in dump.targets]
-        )
+        self.assertListEqual(['vegetation'], [t.name for t in dump.targets])
 
     def test_wrong_style(self):
         alg = PrepareRegressionDatasetFromContinuousVectorAlgorithm()
-        parameters = {
-            alg.P_FEATURE_RASTER: enmap,
-            alg.P_CONTINUOUS_VECTOR: landcover_polygon,
-            alg.P_OUTPUT_DATASET: self.filename('sample.pkl')
-        }
+        parameters = {alg.P_FEATURE_RASTER: enmap, alg.P_CONTINUOUS_VECTOR: landcover_polygon,
+                      alg.P_OUTPUT_DATASET: self.filename('sample.pkl')}
         try:
             self.runalg(alg, parameters)
         except QgsProcessingException as error:
@@ -83,13 +64,9 @@ class TestPrepareRegressionDatasetFromContinuousVectorAlgorithm_2(TestCase):
 
         props = QgsRasterLayerSpectralProperties.fromRasterLayer(enmap_potsdam)
         s = ""
-        parameters = {
-            alg.P_FEATURE_RASTER: enmap_potsdam,
-            alg.P_CONTINUOUS_VECTOR: veg_cover_fraction_potsdam_point,
-            alg.P_TARGET_FIELDS: ['vegetation_fraction'],
-            alg.P_EXCLUDE_BAD_BANDS: True,
-            alg.P_OUTPUT_DATASET: self.filename('sample.pkl')
-        }
+        parameters = {alg.P_FEATURE_RASTER: enmap_potsdam, alg.P_CONTINUOUS_VECTOR: veg_cover_fraction_potsdam_point,
+                      alg.P_TARGET_FIELDS: ['vegetation_fraction'], alg.P_EXCLUDE_BAD_BANDS: True,
+                      alg.P_OUTPUT_DATASET: self.filename('sample.pkl')}
         self.runalg(alg, parameters)
         dump = RegressorDump(**Utils.pickleLoad(parameters[alg.P_OUTPUT_DATASET]))
         self.assertEqual(218, dump.X.shape[1])
@@ -97,13 +74,9 @@ class TestPrepareRegressionDatasetFromContinuousVectorAlgorithm_2(TestCase):
 
     def test_notExcludeBadBands(self):
         alg = PrepareRegressionDatasetFromContinuousVectorAlgorithm()
-        parameters = {
-            alg.P_FEATURE_RASTER: enmap_potsdam,
-            alg.P_CONTINUOUS_VECTOR: landcover_potsdam_point,
-            alg.P_TARGET_FIELDS: ['level_1', 'level_2'],
-            alg.P_EXCLUDE_BAD_BANDS: False,
-            alg.P_OUTPUT_DATASET: self.filename('sample.pkl')
-        }
+        parameters = {alg.P_FEATURE_RASTER: enmap_potsdam, alg.P_CONTINUOUS_VECTOR: landcover_potsdam_point,
+                      alg.P_TARGET_FIELDS: ['level_1', 'level_2'], alg.P_EXCLUDE_BAD_BANDS: False,
+                      alg.P_OUTPUT_DATASET: self.filename('sample.pkl')}
         self.runalg(alg, parameters)
         dump = RegressorDump(**Utils.pickleLoad(parameters[alg.P_OUTPUT_DATASET]))
         self.assertEqual(224, dump.X.shape[1])
