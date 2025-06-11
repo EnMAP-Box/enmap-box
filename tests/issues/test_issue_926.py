3a249493159e5790927e6fef02674e06c77c0af0
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2025-06-11 17:21:44 +0200

    cleanup, remvoed specmix

---
+++ tests/issues/test_issue_926.py
@@ -0,0 +1,19 @@
+from enmapbox.qgispluginsupport.qps import registerSpectralLibraryIOs
+from enmapbox.qgispluginsupport.qps.speclib.core.spectrallibrary import SpectralLibraryUtils
+from enmapbox.testing import start_app, TestCase
+from enmapboxtestdata import landcover_point
+from qgis.core import QgsVectorLayer
+
+start_app()
+registerSpectralLibraryIOs()
+
+
+class TestCaseIssue926(TestCase):
+
+    def test_issue926(self):
+        layer = QgsVectorLayer(landcover_point)
+        tmp = self.createTestOutputDirectory()
+        filenameCopy = tmp / 'copy.geojson'
+        SpectralLibraryUtils.writeToSource(layer, filenameCopy.as_posix())
+        layerCopy = QgsVectorLayer(filenameCopy.as_posix())
+        self.assertTrue(layerCopy.isValid())
