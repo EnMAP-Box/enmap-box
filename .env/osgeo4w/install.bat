3bad0d43d72255a38746790aa0414b753cfc423b
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2025-07-01 17:00:26 +0200

    replaced bitadmin with curl adresses #1182

--- .env/osgeo4w/install.bat
+++ .env/osgeo4w/install.bat
@@ -1,24 +1,51 @@
 :: This installs QGIS + required packages using the OSGeo4W installer
 echo "Install QGIS with EnMAP-Box packages"
 
+@echo off
+rem -------------------------------------------------------------------
+rem  Install QGIS + Python packages with the OSGeo4W v2 network installer
+rem -------------------------------------------------------------------
+
+rem ---- Configuration -------------------------------------------------
 set URL_INSTALLER=https://download.osgeo.org/osgeo4w/v2/osgeo4w-setup.exe
 set PATH_INSTALLER=%USERPROFILE%\Downloads\osgeo4w-setup.exe
 :: available download mirrors? see: https://download.osgeo.org/osgeo4w/ssl/mirrors.lst
 set OSGEO4W_MIRROR=https://download.osgeo.org/osgeo4w/v2
 set OSGEO4W_ARCH=x86_64
 set OSGEO4W_ROOT=F:\OSGEO4W_TEST
+
+:: QGIS package, e.g. qgis, qgis-ltr, qgis-dev
 set QGIS_RELEASE=qgis
 
 if not exist %PATH_INSTALLER% (
-    bitsadmin /transfer "Download osgeo4w-setup.exe" /download /priority normal %URL_INSTALLER% %PATH_INSTALLER%
+    curl -L -o "%PATH_INSTALLER%" "%URL_INSTALLER%"
     echo "Downloaded osgeo4w-setup.exe to %PATH_INSTALLER%"
 ) else (
-    echo 'File exists'
+    echo 'Installer already exists'
 )
 
 :: Packages to install. Available package names see https://download.osgeo.org/osgeo4w/v2/x86_64/setup.ini
-set PACKAGES=python3-pip %QGIS_RELEASE% setup python3-scipy python3-scikit-learn python3-pip python3-geopandas python3-matplotlib python3-pyopengl
+set "PACKAGES=%QGIS_RELEASE%,python3-pip,python3-scipy,python3-scikit-learn,python3-geopandas,python3-matplotlib,python3-pyopengl"
 
-start /B /wait %PATH_INSTALLER% -kq -P %PACKAGES% --root %OSGEO4W_ROOT% --advanced --arch x86_64 --site %OSGEO4W_MIRROR% --only-site --autoaccept --upgrade-also --quiet-mode
-:: start /B /wait %PATH_INSTALLER% -kq -P python3-pip --root %OSGEO4W_ROOT% --advanced --arch x86_64 --site %OSGEO4W_MIRROR% --only-site --autoaccept --upgrade-also --quiet-mode
-echo "QGIS installed to %OSGEO4W_ROOT%"
+"%PATH_INSTALLER%" ^
+    --advanced ^
+    --arch %OSGEO4W_ARCH% ^
+    --root "%OSGEO4W_ROOT%" ^
+    --site "%OSGEO4W_MIRROR%" ^
+    --only-site ^
+    --autoaccept ^
+    --upgrade-also ^
+    --quiet-mode ^
+    --packages %PACKAGES%
+
+if errorlevel 1 (
+    echo ERROR: OSGeo4W installer returned an error & exit /b 1
+)
+
+::start /B /wait %PATH_INSTALLER% -kq -P setup python3-pip --root %OSGEO4W_ROOT% --advanced --arch x86_64 --site %OSGEO4W_MIRROR% --only-site --autoaccept --upgrade-also --quiet-mode
+
+::call %OSGEO4W_ROOT%\OSGeo4W.bat
+::call setup -kq -P -P %PACKAGES% --root %OSGEO4W_ROOT% --advanced --arch x86_64 --site %OSGEO4W_MIRROR% --only-site --autoaccept --upgrade-also --quiet-mode
+::echo "QGIS installed to %OSGEO4W_ROOT%"
+
+
