0145cf52083cac9c5b54097cc8c16781b77ba7dc
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2025-08-01 16:41:07 +0200

    fixed  "Could not find a suitable TLS CA certificate bundle" error in LocationBrowserApp(TestCase)
    Signed-off-by: jakimowb <benjamin.jakimow@geo.hu-berlin.de>

--- tests/enmap-box/enmapbox/eo4qapps/locationbrowserapp/test_LocationBrowserApp.py
+++ tests/enmap-box/enmapbox/eo4qapps/locationbrowserapp/test_LocationBrowserApp.py
@@ -1,4 +1,5 @@
 import requests
+
 from enmapboxprocessing.testcase import TestCase
 
 
@@ -7,5 +8,6 @@ class LocationBrowserApp(TestCase):
     def test_nominatim(self):
         url = 'https://nominatim.openstreetmap.org/search?q=berlin&limit=50&extratags=1&polygon_geojson=1&format=json'
         headers = {"User-Agent": "EnMAP-Box QGIS Plugin (enmapbox@enmap.org)"}  # Required user agent
-        nominatimResults = requests.get(url, headers=headers)
+        import certifi
+        nominatimResults = requests.get(url, headers=headers, verify=certifi.where())
         self.assertEqual(nominatimResults.status_code, 200)
