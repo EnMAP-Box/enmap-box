19400104e9d5bd586e90271465e65eae1dff592e
thomaslfEO <leon-friedrich.thomas@helsinki.fi>
2025-10-28 21:40:24 +0200

    test for deep learning trainer, extra clean up before running unet test

--- tests/enmap-box/enmapbox/apps/SpecDeepMap/test_deep_learning_trainer.py
+++ tests/enmap-box/enmapbox/apps/SpecDeepMap/test_deep_learning_trainer.py
@@ -53,7 +53,7 @@ class Test_Deep_Learning_Trainer(TestCase):
         folder_path_input = BASE_TESTDATA / 'test_requierments'
         self.assertTrue(folder_path_input.is_dir())
         # folder_path = BASE_DIR / "test_run"
-        folder_path = self.createTestOutputDirectory()
+        folder_path = self.createTestOutputDirectory(cleanup=True)
         self.assertTrue(folder_path.is_dir(), msg=f'Dir does not exists: {folder_path}')
 
         folder_path = folder_path.as_posix()
@@ -133,7 +133,15 @@ class Test_Deep_Learning_Trainer(TestCase):
         # Get the script's directory (makes paths relative)
         # BASE_DIR = dirname(__file__)
         # folder_path = join(BASE_DIR, "test_run")
-        folder_path = self.createTestOutputDirectory()
+        folder_path_unet = self.createTestOutputDirectory(cleanup=True)
+        folder_path_unet = folder_path_unet.as_posix()
+
+        for filename in os.listdir(folder_path_unet):
+            if filename.endswith(".ckpt"):  # Assuming checkpoint files have a .ckpt extension
+                file_path = os.path.join(folder_path_unet, filename)
+                os.remove(file_path)
+
+
         folder_path_input = BASE_TESTDATA / 'test_requierments'
 
         # 4. Test check if pretrained model runs needs 13 channel adjust indexing before splitting raster
@@ -155,13 +163,13 @@ class Test_Deep_Learning_Trainer(TestCase):
               alg.device_numbers: 1,
               alg.num_models: -1,
               alg.tensorboard: False,
-              alg.logdirpath: folder_path.as_posix(),
-              alg.logdirpath_model: folder_path.as_posix(),
+              alg.logdirpath: folder_path_unet,
+              alg.logdirpath_model: folder_path_unet,
               }
 
         result = Processing.runAlgorithm(alg, parameters=io)
 
-        best = best_ckpt_path(folder_path)
+        best = best_ckpt_path(folder_path_unet)
 
         model_loaded = MyModel.load_from_checkpoint(best)
 
@@ -186,7 +194,7 @@ class Test_Deep_Learning_Trainer(TestCase):
         # assert frozen, "Error: The model's backbone is not frozen."
 
         # delete checkpoint files from previous test
-        for filename in os.listdir(folder_path):
+        for filename in os.listdir(folder_path_unet):
             if filename.endswith(".ckpt"):  # Assuming checkpoint files have a .ckpt extension
-                file_path = os.path.join(folder_path, filename)
+                file_path = os.path.join(folder_path_unet, filename)
                 os.remove(file_path)
