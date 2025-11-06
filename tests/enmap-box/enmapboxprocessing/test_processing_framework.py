e48b8f432034082d822f428899a7330d653bfa94
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2025-11-06 15:42:19 +0100

    fixed test

--- tests/enmap-box/enmapboxprocessing/test_processing_framework.py
+++ tests/enmap-box/enmapboxprocessing/test_processing_framework.py
@@ -1,7 +1,11 @@
 import unittest
 
+from enmapbox import initAll
 from enmapbox.gui.enmapboxgui import EnMAPBox
-from enmapbox.testing import TestCase
+from enmapbox.testing import TestCase, start_app
+
+start_app()
+initAll()
 
 
 class TestProcessingFramework(TestCase):
@@ -9,4 +13,5 @@ class TestProcessingFramework(TestCase):
     @unittest.skipIf(TestCase.runsInCI(), 'Blocking Dialog')
     def test_start_algorithm(self):
         EMB = EnMAPBox(load_core_apps=False, load_other_apps=False)
+        EMB.loadExampleData()
         EMB.executeAlgorithm('gdal:translate', EMB.ui)
