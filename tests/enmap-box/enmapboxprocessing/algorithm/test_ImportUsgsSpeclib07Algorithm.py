c3b5ac6da3d8cc04da3577a7664903683f761f33
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2026-02-26 14:16:52 +0100

    resolved #1468

--- tests/enmap-box/enmapboxprocessing/algorithm/test_ImportUsgsSpeclib07Algorithm.py
+++ tests/enmap-box/enmapboxprocessing/algorithm/test_ImportUsgsSpeclib07Algorithm.py
@@ -41,3 +41,59 @@ class TestImportUsgsSpeclib07Algorithm(TestCase):
         self.runalg(alg, parameters)
         library = QgsVectorLayer(parameters[alg.P_OUTPUT_LIBRARY])
         self.assertEqual(2457, library.featureCount())
+
+    def test_OversampledCubicSplineInterpolationCharacteristic(self):
+        if speclibProductsRoot() is None or self.skipProductImport:
+            return
+
+        initAll()
+
+        alg = ImportUsgsSpeclib07Algorithm()
+        parameters = {
+            alg.P_FOLDER: SpeclibProducts.UsgsSplib07.folder,
+            alg.P_SPECTROMETER: alg.AllSpectrometers,
+            alg.P_CHAPTER: alg.AllChapters,
+            alg.P_SPECTRAL_CHARACTERISTIC: alg.OversampledCubicSplineInterpolationCharacteristic,
+            alg.P_OUTPUT_LIBRARY: self.filename('usgsSplib07.gpkg')
+        }
+        self.runalg(alg, parameters)
+        library = QgsVectorLayer(parameters[alg.P_OUTPUT_LIBRARY])
+        self.assertEqual(2457, library.featureCount())
+
+    def test_Landsat8OLICharacteristic(self):
+        if speclibProductsRoot() is None or self.skipProductImport:
+            return
+
+        initAll()
+
+        alg = ImportUsgsSpeclib07Algorithm()
+        parameters = {
+            alg.P_FOLDER: SpeclibProducts.UsgsSplib07.folder,
+            alg.P_SPECTROMETER: alg.AllSpectrometers,
+            alg.P_CHAPTER: alg.AllChapters,
+            alg.P_SPECTRAL_CHARACTERISTIC: alg.Landsat8OLICharacteristic,
+            alg.P_OUTPUT_LIBRARY: self.filename('usgsSplib07.gpkg')
+        }
+        self.runalg(alg, parameters)
+        library = QgsVectorLayer(parameters[alg.P_OUTPUT_LIBRARY])
+
+    def test_all_characteristics(self):
+        if speclibProductsRoot() is None or self.skipProductImport:
+            return
+
+        initAll()
+
+        alg = ImportUsgsSpeclib07Algorithm()
+        for characteristic in alg.AllCharacteristics:
+            print('+++', alg.O_SPECTRAL_CHARACTERISTIC[characteristic], '+++')
+            parameters = {
+                alg.P_FOLDER: SpeclibProducts.UsgsSplib07.folder,
+                alg.P_SPECTROMETER: alg.AllSpectrometers,
+                alg.P_CHAPTER: alg.AllChapters,
+                alg.P_SPECTRAL_CHARACTERISTIC: characteristic,
+                alg.P_OUTPUT_LIBRARY: self.filename(
+                    str(characteristic).zfill(2) + '_' + alg.O_SPECTRAL_CHARACTERISTIC[characteristic] + '.gpkg'
+                )
+            }
+            self.runalg(alg, parameters)
+            library = QgsVectorLayer(parameters[alg.P_OUTPUT_LIBRARY])
