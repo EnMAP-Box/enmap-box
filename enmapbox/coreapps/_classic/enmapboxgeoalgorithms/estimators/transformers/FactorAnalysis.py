6062566d09445616bd532b73a29de5c9e1d0c736
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2026-06-12 09:52:41 +0200

    removed deprecated Classification Workflow app

--- enmapbox/coreapps/_classic/enmapboxgeoalgorithms/estimators/transformers/FactorAnalysis.py
+++
@@ -1,6 +0,0 @@
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import FactorAnalysis
-
-factorAnalysis = FactorAnalysis(n_components=3)
-estimator = make_pipeline(StandardScaler(), factorAnalysis)
--- enmapbox/coreapps/_classic/enmapboxgeoalgorithms/estimators/transformers/MaxAbsScaler.py
+++
@@ -1,3 +0,0 @@
-from sklearn.preprocessing import MaxAbsScaler
-
-estimator = MaxAbsScaler()
