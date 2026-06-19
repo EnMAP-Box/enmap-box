9fa615133f062ef7deac74624fdba5e9aa2f3013
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2026-06-19 16:28:01 +0200

    systeminfo uses packages from requirements.csv if available

--- scripts/systeminfo.py
+++ scripts/systeminfo.py
@@ -1,5 +1,7 @@
+import csv
 import platform
 import sys
+from pathlib import Path
 
 from qgis.core import Qgis
 
@@ -12,10 +14,24 @@ print(f'Sys exe: {sys.executable}')
 print(f'QGIS: {Qgis.version()} {Qgis.devVersion()}')
 
 print('Package locations:')
-packages = ['osgeo.gdal', 'numpy', 'scipy',
-            'sklearn', 'OpenGL', 'ee',
-            'matplotlib', 'pip', 'astropy', 'xgboost', 'lightgbm', 'catboost',
-            'sympy', 'numba', 'netCDF4', 'enpt-enmapboxapp', ]
+
+packageinfo = Path(__file__).parents[1] / '.env' / 'requirements.csv'
+if packageinfo.is_file():
+    packages = []
+    with open(packageinfo, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            pkg = row['py_name'] if row['py_name'] else row['pip_name']
+            pkg = pkg.strip()
+            if pkg != '':
+                packages.append(pkg)
+            else:
+                s = ""
+else:
+    packages = ['osgeo.gdal', 'numpy', 'scipy',
+                'sklearn', 'OpenGL', 'ee',
+                'matplotlib', 'pip', 'astropy', 'xgboost', 'lightgbm', 'catboost',
+                'sympy', 'numba', 'netCDF4', 'enpt_enmapboxapp', ]
 for p in packages:
     try:
         pkg = __import__(p)
