e714eaf20b1652e396b51cc68d660141b52cd104
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2026-07-22 10:16:31 +0200

    Create test_profileanalyticsapp.py

---
+++ tests/enmap-box/enmapbox/eo4qapps/profileanalyticsapp/test_profileanalyticsapp.py
@@ -0,0 +1,32 @@
+from enmapbox import initAll
+from enmapbox.gui.enmapboxgui import EnMAPBox
+from enmapbox.testing import start_app
+from enmapboxprocessing.testcase import TestCase
+from enmapboxtestdata import enmap
+from profileanalyticsapp import ProfileAnalyticsDockWidget
+from qgis.PyQt.QtWidgets import QDockWidget
+from qgis.core import QgsRasterLayer
+
+qgsApp = start_app()
+initAll()
+
+
+class TestLocationBrowserDockWidget(TestCase):
+
+    def test(self):
+        enmapBox = EnMAPBox(None)
+        layer = QgsRasterLayer(enmap, 'enmap_berlin')
+        enmapBox.onDataDropped([layer])
+
+        for widget in enmapBox.ui.findChildren(QDockWidget):
+            if isinstance(widget, ProfileAnalyticsDockWidget):
+                break
+
+        self.assertIsInstance(widget, ProfileAnalyticsDockWidget)
+        widget.show()
+
+        if False:
+            qgsApp.exec()
+
+        self.dispose_widget(widget)
+        self.dispose_widget(enmapBox.ui)
