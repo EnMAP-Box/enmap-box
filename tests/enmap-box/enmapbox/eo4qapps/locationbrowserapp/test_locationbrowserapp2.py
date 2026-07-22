450744e9eae9f728dbffb3254b38daafc12e83f0
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2026-07-22 10:13:26 +0200

    Create test_locationbrowserapp2.py

---
+++ tests/enmap-box/enmapbox/eo4qapps/locationbrowserapp/test_locationbrowserapp2.py
@@ -0,0 +1,33 @@
+from enmapbox import initAll
+from enmapbox.gui.enmapboxgui import EnMAPBox
+from enmapbox.testing import start_app
+from enmapboxprocessing.testcase import TestCase
+from enmapboxtestdata import enmap
+from locationbrowserapp import LocationBrowserDockWidget
+from qgis.PyQt.QtWidgets import QDockWidget
+from qgis.core import QgsRasterLayer
+
+qgsApp = start_app()
+initAll()
+
+
+class TestLocationBrowserDockWidget(TestCase):
+
+    def test(self):
+        enmapBox = EnMAPBox(None)
+        layer = QgsRasterLayer(enmap, 'enmap_berlin')
+        enmapBox.onDataDropped([layer])
+
+        for widget in enmapBox.ui.findChildren(QDockWidget):
+            if isinstance(widget, LocationBrowserDockWidget):
+                break
+
+        self.assertIsInstance(widget, LocationBrowserDockWidget)
+        widget.show()
+        widget.onRequestNominatimClicked()
+
+        if not False:
+            qgsApp.exec()
+
+        self.dispose_widget(widget)
+        self.dispose_widget(enmapBox.ui)
