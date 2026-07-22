d20e812cfaff1eaf8205730a2e23855741e6e5f8
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2026-07-22 09:38:45 +0200

    Create test_geetimeseriesexplorerapp.py

---
+++ tests/enmap-box/enmapbox/eo4qapps/geetimeseriesexplorerapp/test_geetimeseriesexplorerapp.py
@@ -0,0 +1,41 @@
+from enmapbox import initAll
+from enmapbox.gui.enmapboxgui import EnMAPBox
+from enmapbox.testing import start_app
+from enmapboxprocessing.testcase import TestCase
+from enmapboxtestdata import enmap
+from geetimeseriesexplorerapp import GeeTimeseriesExplorerDockWidget, GeeTemporalProfileDockWidget
+from qgis.PyQt.QtWidgets import QDockWidget
+from qgis.core import QgsRasterLayer
+
+qgsApp = start_app()
+initAll()
+
+
+class TestGeeTimeseriesExplorerApp(TestCase):
+
+    def test(self):
+        enmapBox = EnMAPBox(None)
+        layer = QgsRasterLayer(enmap, 'enmap_berlin')
+        enmapBox.onDataDropped([layer])
+
+        for widget1 in enmapBox.ui.findChildren(QDockWidget):
+            print(widget1)
+            if isinstance(widget1, GeeTimeseriesExplorerDockWidget):
+                break
+
+        self.assertIsInstance(widget1, GeeTimeseriesExplorerDockWidget)
+        widget1.show()
+
+        for widget2 in enmapBox.ui.findChildren(QDockWidget):
+            if isinstance(widget2, GeeTemporalProfileDockWidget):
+                break
+
+        self.assertIsInstance(widget2, GeeTemporalProfileDockWidget)
+        widget2.show()
+
+        if not False:
+            qgsApp.exec()
+
+        self.dispose_widget(widget1)
+        self.dispose_widget(widget2)
+        self.dispose_widget(enmapBox.ui)
