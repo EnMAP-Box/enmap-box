199e36bb15faed60e8f1f3ce4977d3a6de311169
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2026-08-27 22:15:50 +0200

    fixed bivariatecolorrasterrendererdialog.py

--- enmapbox/coreapps/bivariatecolorrasterrendererapp/bivariatecolorrasterrendererdialog.py
+++ enmapbox/coreapps/bivariatecolorrasterrendererapp/bivariatecolorrasterrendererdialog.py
@@ -2,23 +2,25 @@ from typing import Optional
 
 import numpy as np
 from pyqtgraph import PlotWidget, ImageItem
-from scipy.interpolate import LinearNDInterpolator
-
-from bivariatecolorrasterrendererapp.bivariatecolorrasterrenderer import BivariateColorRasterRenderer
-from enmapbox.qgispluginsupport.qps.utils import SpatialExtent
-from enmapboxprocessing.rasterreader import RasterReader
 from qgis.PyQt.QtGui import QColor
-from qgis.PyQt.QtWidgets import QWidget, QToolButton, QCheckBox, QMainWindow, QComboBox, QMenu, QAction, QPushButton, \
+from qgis.PyQt.QtWidgets import QToolButton, QCheckBox, QMainWindow, QComboBox, QMenu, QAction, QPushButton, \
     QLineEdit, QRadioButton
 from qgis.PyQt.uic import loadUi
 from qgis.core import QgsRasterLayer, QgsMapLayerProxyModel, QgsMapSettings
 from qgis.gui import QgsMapCanvas, QgsRasterBandComboBox, QgsDoubleSpinBox, QgsMapLayerComboBox, QgsColorButton, \
     QgsSpinBox
+from scipy.interpolate import LinearNDInterpolator
+
+from bivariatecolorrasterrendererapp.bivariatecolorrasterrenderer import BivariateColorRasterRenderer
+from enmapbox.qgispluginsupport.qps.utils import SpatialExtent
+from enmapboxprocessing.rasterreader import RasterReader
 
 
 class ColorPlanePlotWidget(PlotWidget):
-    def __init__(self, *args):
-        PlotWidget.__init__(self, parent=None, background='#f0f0f0')
+    def __init__(self, *args, **kwds):
+        if 'background' not in kwds:
+            kwds['background'] = '#f0f0f0'
+        super().__init__(*args, **kwds)
         self.setDefaultPadding(padding=0.02)
 
 
@@ -53,8 +55,8 @@ class BivariateColorRasterRendererDialog(QMainWindow):
     EstimatedAccuracy, ActualAccuracy = 0, 1
     WholeRasterExtent, CurrentCanvasExtent = 0, 1
 
-    def __init__(self, parent=None):
-        QWidget.__init__(self, parent)
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
         loadUi(__file__.replace('.py', '.ui'), self)
 
         from enmapbox.gui.enmapboxgui import EnMAPBox
--- tests/enmap-box/enmapbox/coreapps/bivariatecolorrasterrendererapp/test_bivariatecolorrasterrenderer.py
+++ tests/enmap-box/enmapbox/coreapps/bivariatecolorrasterrendererapp/test_bivariatecolorrasterrenderer.py
@@ -1,10 +1,11 @@
+from qgis.core import QgsRasterLayer
+
 from bivariatecolorrasterrendererapp import BivariateColorRasterRendererDialog
 from enmapbox import initAll
 from enmapbox.gui.enmapboxgui import EnMAPBox
 from enmapbox.testing import start_app
 from enmapboxprocessing.testcase import TestCase
 from enmapboxtestdata import enmap
-from qgis.core import QgsRasterLayer
 
 qgsApp = start_app()
 initAll()
@@ -13,7 +14,7 @@ initAll()
 class TestBivariateColorRasterRendererApp(TestCase):
 
     def test(self):
-        enmapBox = EnMAPBox(None)
+        enmapBox = EnMAPBox()
         layer = QgsRasterLayer(enmap, 'enmap_berlin.bsq')
         enmapBox.onDataDropped([layer])
 
@@ -21,8 +22,10 @@ class TestBivariateColorRasterRendererApp(TestCase):
         widget.show()
         widget.mLayer.setLayer(layer)
 
-        if False:
-            qgsApp.exec()
+        self.showGui([enmapBox.ui, widget])
+        enmapBox.close()
+        # if False:
+        #    qgsApp.exec()
 
-        self.dispose_widget(widget)
-        self.dispose_widget(enmapBox.ui)
+        # self.dispose_widget(widget)
+        # self.dispose_widget(enmapBox.ui)
