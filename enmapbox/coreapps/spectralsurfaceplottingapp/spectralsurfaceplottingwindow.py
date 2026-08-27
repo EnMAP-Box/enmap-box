f4d6fd05dc6bb9dfe5d27e6e2df73eaef1bb1a39
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2026-08-27 10:41:44 +0200

    adressed #1561

--- enmapbox/coreapps/spectralsurfaceplottingapp/spectralsurfaceplottingwindow.py
+++ enmapbox/coreapps/spectralsurfaceplottingapp/spectralsurfaceplottingwindow.py
@@ -1,21 +1,18 @@
 import numpy as np
 import pyvista as pv
 from pyvistaqt import QtInteractor
-from qgis.PyQt.QtWidgets import QSizePolicy
-from qgis.PyQt.QtWidgets import QSlider
-from qgis.PyQt.QtWidgets import QToolButton, QMainWindow, QComboBox, QCheckBox
-from qgis.PyQt.QtWidgets import QVBoxLayout
-from qgis.PyQt.QtWidgets import QWidget
+from qgis.PyQt.QtWidgets import QSizePolicy, QSlider, QToolButton, QMainWindow, QComboBox, QCheckBox, QVBoxLayout, \
+    QWidget
 from qgis.PyQt.uic import loadUi
-from qgis.core import QgsMapLayerProxyModel, QgsRasterLayer
-from qgis.gui import QgsMapLayerComboBox, QgsFieldComboBox
+from qgis.core import QgsColorRamp, QgsStyle, QgsMapLayerProxyModel, QgsRasterLayer
+from qgis.gui import QgsColorRampButton, QgsMessageBar, QgsMapLayerComboBox, QgsFieldComboBox
 from vtkmodules.vtkCommonCore import vtkStringArray
 
 from enmapboxprocessing.libraryreader import LibraryReader
-from spectralsurfaceplottingapp.exampledata import getRandomData
 
 
 class SpectralSurfacePlottingWindow(QMainWindow):
+    mMessageBar: QgsMessageBar
     mPlot: QWidget
 
     mDataFormat: QComboBox
@@ -26,13 +23,15 @@ class SpectralSurfacePlottingWindow(QMainWindow):
     mShowEdges: QCheckBox
     mShowGrid: QCheckBox
     mShowAxes: QCheckBox
+    mColorRamp: QgsColorRampButton
 
     mFieldLfX: QgsFieldComboBox
     mFieldLfY: QgsFieldComboBox
     mFieldLfZ: QgsFieldComboBox
-    mLoadData: QToolButton
     mFieldLibraryProfiles: QgsFieldComboBox
     mFieldLibraryY: QgsFieldComboBox
+    mFieldLibraryC: QgsFieldComboBox
+    mLoadData: QToolButton
 
     scaleBase = 2
     mScaleX: QSlider
@@ -45,6 +44,9 @@ class SpectralSurfacePlottingWindow(QMainWindow):
     def __init__(self, *args, **kwds):
         QMainWindow.__init__(self, *args, **kwds)
         loadUi(__file__.replace('.py', '.ui'), self)
+
+        self.meshActorSurface = None
+        self.meshActorPoints = None
 
         self.plotLayout = QVBoxLayout(self.mPlot)
         self.plotLayout.setContentsMargins(0, 0, 0, 0)
@@ -73,6 +75,7 @@ class SpectralSurfacePlottingWindow(QMainWindow):
         self.mShowEdges.checkStateChanged.connect(self.onShowEdgesChanged)
         self.mShowGrid.checkStateChanged.connect(self.onShowGridChanged)
         self.mShowAxes.checkStateChanged.connect(self.onShowAxesChanged)
+        self.mColorRamp.colorRampChanged.connect(self.onColorRampChanged)
 
         self.mScaleX.valueChanged.connect(self.onScaleChanged)
         self.mScaleY.valueChanged.connect(self.onScaleChanged)
@@ -80,6 +83,9 @@ class SpectralSurfacePlottingWindow(QMainWindow):
         self.mAutoScale.clicked.connect(self.onAutoScale)
 
         self.mLoadData.clicked.connect(self.onLoadData)
+
+        colorRamp: QgsColorRamp = QgsStyle().defaultStyle().colorRamp('Turbo')
+        self.mColorRamp.setColorRamp(colorRamp)
 
         self.scaleX = 1
         self.scaleY = 1
@@ -91,6 +97,8 @@ class SpectralSurfacePlottingWindow(QMainWindow):
         x = list()
         y = list()
         z = list()
+        c = list()
+
         if self.mDataFormat.currentIndex() == self.LongFormat:
             fieldX = self.mFieldLfX.currentField()
             fieldY = self.mFieldLfY.currentField()
@@ -104,23 +112,30 @@ class SpectralSurfacePlottingWindow(QMainWindow):
             reader = LibraryReader(layer)
             fieldProfile = self.mFieldLibraryProfiles.currentField()
             fieldY = self.mFieldLibraryY.currentField()
-
-            for i, (values, geometry) in enumerate(reader.data(), 1):
-                xs = values[fieldProfile]['x']
-                zs = values[fieldProfile]['y']
-                for xi, zi in zip(xs, zs):
+            fieldC = self.mFieldLibraryC.currentField()
+            try:
+                for i, (values, geometry) in enumerate(reader.data(), 1):
+                    xs = values[fieldProfile]['x']
+                    zs = values[fieldProfile]['y']
                     yi = values.get(fieldY, i)
-                    if not np.isfinite([xi, yi, zi]).all():
-                        continue
-                    x.append(xi)
-                    y.append(yi)
-                    z.append(zi)
+
+                    for xi, zi in zip(xs, zs):
+                        if not np.isfinite([xi, yi, zi]).all():
+                            continue
+                        ci = values.get(fieldC, zi)  # default color is zi
+                        x.append(xi)
+                        y.append(yi)
+                        z.append(zi)
+                        c.append(ci)
+            except Exception:
+                self.mMessageBar.pushWarning('Load data', 'select a spectral profile attribute')
+
         else:
             raise ValueError()
 
-        return x, y, z
+        return x, y, z, c
 
-    def setData(self, x, y, z):
+    def setData(self, x, y, z, c):
         self.x = np.array(x, dtype=float)
         self.y = np.array(y, dtype=float)
         self.z = np.array(z, dtype=float)
@@ -128,11 +143,14 @@ class SpectralSurfacePlottingWindow(QMainWindow):
         if not np.isfinite(xyz).all():
             raise ValueError('Spectral Surface Plotting: data includes non-finite values')
         self.point_cloud = pv.PolyData(xyz)
-        self.point_cloud.point_data['Z'] = z
+        self.point_cloud.point_data["C"] = c
+
         self.mesh = self.point_cloud.delaunay_2d()
-        self.mesh.point_data["Z"] = self.mesh.points[:, 2]
+        self.mesh.point_data["C"] = c
 
     def updateGrid(self):
+        if self.meshActorSurface is None:
+            return
 
         # remove previous CubeAxesActor
         self.plotter.remove_bounds_axes()
@@ -170,21 +188,21 @@ class SpectralSurfacePlottingWindow(QMainWindow):
 
         self.meshActorSurface = self.plotter.add_mesh(
             self.mesh,
-            scalars="Z",
+            scalars="C",
             cmap="viridis",
             show_edges=self.mShowEdges.isChecked(),
             edge_color="black",
             line_width=0.4,
             smooth_shading=True,
             opacity=0.8 + 0.2,
-            scalar_bar_args={"title": "Z"},
+            scalar_bar_args={"title": "C"},
             name='surface'
         )
         self.onShowSurfaceChanged()
 
         self.meshActorPoints = self.plotter.add_mesh(
             self.point_cloud,
-            scalars="Z",
+            scalars="C",
             cmap="viridis",
             render_points_as_spheres=True,
             point_size=8,
@@ -205,6 +223,9 @@ class SpectralSurfacePlottingWindow(QMainWindow):
         # self.plotter.enable_parallel_projection()
 
     def autoScaleFactors(self):
+        if self.meshActorSurface is None:
+            return
+
         x_range = np.ptp(self.x)
         y_range = np.ptp(self.y)
         z_range = np.ptp(self.z)
@@ -223,29 +244,66 @@ class SpectralSurfacePlottingWindow(QMainWindow):
         return xscale, yscale, zscale
 
     def setScale(self, xscale, yscale, zscale):
+        if self.meshActorSurface is None:
+            return
+
         self.plotter.remove_bounds_axes()
         self.plotter.set_scale(xscale, yscale, zscale, reset_camera=True)
         self.updateGrid()
 
     def onShowSurfaceChanged(self):
+        if self.meshActorSurface is None:
+            return
+
         self.meshActorSurface.SetVisibility(self.mShowSurface.isChecked())
 
     def onShowPointsChanged(self):
+        if self.meshActorSurface is None:
+            return
+
         self.meshActorPoints.SetVisibility(self.mShowPoints.isChecked())
 
     def onShowEdgesChanged(self):
+        if self.meshActorSurface is None:
+            return
+
         self.meshActorSurface.prop.show_edges = self.mShowEdges.isChecked()
         self.plotter.render()
 
     def onShowGridChanged(self):
+        if self.meshActorSurface is None:
+            return
+
         self.gridActor.SetVisibility(self.mShowGrid.isChecked())
         self.plotter.render()
 
     def onShowAxesChanged(self):
+        if self.meshActorSurface is None:
+            return
+
         self.axesActor.SetVisibility(self.mShowAxes.isChecked())
         self.plotter.render()
 
+    def onColorRampChanged(self):
+        if self.meshActorSurface is None:
+            return
+
+        ramp = self.mColorRamp.colorRamp()
+        n = 256
+        cmap = []
+        for i in range(n):
+            value = i / (n - 1)
+            color = ramp.color(value).name()
+            cmap.append(color)
+
+        self.meshActorSurface.mapper.lookup_table.apply_cmap(cmap, n_values=256)
+        self.meshActorPoints.mapper.lookup_table.apply_cmap(cmap, n_values=256)
+        self.plotter.render()
+
     def onScaleChanged(self):
+        if self.meshActorSurface is None:
+            return
+
         self.scaleX = self.scaleBase ** self.mScaleX.value()
         self.scaleY = self.scaleBase ** self.mScaleY.value()
         self.scaleZ = self.scaleBase ** self.mScaleZ.value()
@@ -253,6 +311,9 @@ class SpectralSurfacePlottingWindow(QMainWindow):
         self.plotter.render()
 
     def onAutoScale(self):
+        if self.meshActorSurface is None:
+            return
+
         xscale, yscale, zscale = self.autoScaleFactors()
         self.mScaleX.setValue(int(np.log(xscale) / np.log(self.scaleBase)))
         self.mScaleY.setValue(int(np.log(yscale) / np.log(self.scaleBase)))
@@ -263,11 +324,16 @@ class SpectralSurfacePlottingWindow(QMainWindow):
         layer: QgsRasterLayer = self.mLayer.currentLayer()
         if layer is None:
             return
-
-            x, y, z = getRandomData()
-        else:
-            x, y, z = self.readData()
-
-        self.setData(y, x, z)
+        x, y, z, c = self.readData()
+        self.setData(y, x, z, c)
         self.plotData()
         self.onAutoScale()
+
+    def closeEvent(self, event):
+        try:
+            if self.plotter is not None:
+                self.plotter.Finalize()
+                self.plotter.close()
+                self.plotter = None
+        finally:
+            super().closeEvent(event)
