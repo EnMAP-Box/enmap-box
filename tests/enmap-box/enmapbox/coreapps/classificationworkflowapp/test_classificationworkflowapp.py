e7324500ee9b2adac50324e41cc1a7eb5fb5dad1
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2026-09-08 10:54:03 +0200

    disables unittest that use QWebEngine if run in offscreen mode

--- tests/enmap-box/enmapbox/coreapps/classificationworkflowapp/test_classificationworkflowapp.py
+++ tests/enmap-box/enmapbox/coreapps/classificationworkflowapp/test_classificationworkflowapp.py
@@ -1,3 +1,6 @@
+import os
+import unittest
+
 from classificationworkflowapp import ClassificationWorkflowGui
 from enmapbox.gui.enmapboxgui import EnMAPBox
 from enmapbox.testing import start_app
@@ -6,7 +9,10 @@ from enmapboxprocessing.testcase import TestCase
 qgsApp = start_app()
 start_app()
 
+runs_offscreen = os.environ.get('QT_QPA_PLATFORM', '').lower() in ['offscreen']
 
+
+@unittest.skipIf(runs_offscreen, 'QT_QPA_PLATFORM=offscreen. QWebEngineView requires a screen device')
 class TestClassificationWorkflowApp(TestCase):
 
     def test_workflowapp(self):
