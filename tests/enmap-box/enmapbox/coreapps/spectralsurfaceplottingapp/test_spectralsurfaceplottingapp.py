1a825b67ff646caf3da8ed7f02b209aa71006eed
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2026-09-08 10:33:10 +0200

    Update test_spectralsurfaceplottingapp.py

--- tests/enmap-box/enmapbox/coreapps/spectralsurfaceplottingapp/test_spectralsurfaceplottingapp.py
+++ tests/enmap-box/enmapbox/coreapps/spectralsurfaceplottingapp/test_spectralsurfaceplottingapp.py
@@ -33,7 +33,6 @@ class TestSpectralSurfacePlottingApp(TestCase):
     def testPlotter(self):
         enmapBox = EnMAPBox()
         x, y, z = getRandomData()
-        x, y, z = getLmuWeizen()
 
         widget = SpectralSurfacePlottingWindow()
         widget.setData(y, x, z)
@@ -50,20 +49,4 @@ def getRandomData():
     x = rng.random(number_of_points) * 100
     y = rng.random(number_of_points) * 100
     z = np.sinc((x - 20) / 100 * np.pi) + np.sinc((y - 50) / 100 * np.pi)
-    return x, y, z
-
-
-def getLmuWeizen():
-    data = np.genfromtxt(r'C:\Users\janzandr\Downloads\STS_Weizen_2017_orig.csv', delimiter=';')
-    wavelength = data[0, 3:]
-    doys = data[2:, 0]
-    values = data[2:, 3:]
-    x = list()
-    y = list()
-    z = list()
-    for xi, doyi in enumerate(doys):
-        for yi, wli in enumerate(wavelength):
-            x.append(doyi)
-            y.append(wli)
-            z.append(values[xi, yi])
     return x, y, z
